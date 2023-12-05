9cf23fd5ecbe6ae272ca3e43c5cd2e0665f0c144
Jack Pfeiffer <jpfeiffer@michiganlabs.com>
2023-12-05 17:10:09 -0500

    Day 05

--- 2023/day-05/solution.py
+++ 2023/day-05/solution.py
@@ -1,37 +1,67 @@
-import re
+DEBUG = False
 
 with open('input.txt') as f:
-    inp = " ".join(f.readlines()).replace("\n", "")
+    lines = f.read()
 
-nums_exp = "([\d ]*)"
-exp = "seeds:" + nums_exp
-exp += "seed-to-soil map:" + nums_exp
-exp += "soil-to-fertilizer map:" + nums_exp
-exp += "fertilizer-to-water map:" + nums_exp
-exp += "water-to-light map:" + nums_exp
-exp += "light-to-temperature map:" + nums_exp
-exp += "temperature-to-humidity map:" + nums_exp
-exp += "humidity-to-location map:" + nums_exp
+[seeds, *maps] = lines.split('\n\n')
 
-e = re.compile(exp)
-[seeds, seed_soil, soil_fert, fert_water, water_light, light_temp, temp_humid, humid_loc] = [i.strip() for i in
-                                                                                             e.findall(inp)[0]]
+functions = []
 
 
-def map_sets(str):
-    sets = str.split(' ')
-    map = {}
-    for set in zip(*[sets[i::3] for i in range(3)]):
+def debug(msg='', end='\n'):
+    if DEBUG:
+        print(msg, end=end)
 
 
+def add_func(name, m):
+    def func(n):
+        for [dst, src, l] in m:
+            if src <= n <= src + l:
+                debug(f"{name}: {dst + n - src}", end=' -> ')
+                return dst + n - src
 
-    return map
+        debug(f"{name}: {n}", end=' -> ')
+        return n
+
+    functions.append(func)
 
 
-seed_soil_map = map_sets(seed_soil)
-soil_fert_map = map_sets(soil_fert)
-fert_water_map = map_sets(fert_water)
-water_light_map = map_sets(water_light)
-light_temp_map = map_sets(light_temp)
-temp_humid_map = map_sets(temp_humid)
-humid_loc_map = map_sets(humid_loc)
+for mapping in maps:
+    name = mapping.split('\n')[0]
+    name = name.split(' ')[0].split('-')[2]
+
+    mapping = [list(map(int, t.split(' '))) for t in mapping.split('\n')[1:]]
+    # mapping.sort(key=lambda x: x[1])
+
+    add_func(name, mapping)
+
+loc_seed = {}
+
+
+def get_loc(seed):
+    loc = seed
+    debug(f"seed: {loc}", end=' -> ')
+    for f in functions:
+        loc = f(loc)
+
+    debug()
+    loc_seed[loc] = seed
+
+    return seed
+
+
+seeds = list(map(int, seeds.split(' ')[1:]))
+print(f"Part 1: {min(map(get_loc, seeds))}")
+
+loc_seed = {}
+# DEBUG = True
+
+mini_ranges = []
+for [start, l] in zip(*[seeds[i::2] for i in range(2)]):
+
+    for seed in range(start, start + l):
+        get_loc(seed)
+print([list(zip(seeds, [1] * len(seeds))), list(zip(seeds[0::2], seeds[1::2]))])
+
+# print(f"Part 2: {min(loc_seed.keys())}")
+print("Done")
