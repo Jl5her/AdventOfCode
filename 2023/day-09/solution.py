24194dc125174a18a8ea168d305148f0237bd8d7
Jack Pfeiffer <jl5her@yahoo.com>
2023-12-13 18:32:40 -0500

    2023 Day 09 Part 2

--- 2023/day-09/solution.py
+++ 2023/day-09/solution.py
@@ -15,5 +15,21 @@ def append_next(seq, s=0):
     return seq
 
 
+def insert_prev(seq, s=0):
+    if list(set(seq)) == [0]:
+        seq.insert(0, 0)
+        return seq
+
+    diff = [seq[i] - seq[i - 1] for i in range(1, len(seq))]
+    diff = insert_prev(diff, s + 1)
+
+    seq.insert(0, seq[0] - diff[0])
+
+    return seq
+
+
 part1 = sum([append_next(seq)[-1] for seq in lines])
 print(f"Part 1: {part1}")
+
+part2 = sum([insert_prev(seq)[0] for seq in lines])
+print(f"Part 2: {part2}")
