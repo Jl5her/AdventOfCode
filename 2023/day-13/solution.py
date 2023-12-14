3c8c46c14dc58f05f38238dfddb18eea18a0f140
Jack Pfeiffer <jl5her@yahoo.com>
2023-12-13 19:23:59 -0500

    2023 Day 13 Part 2

--- 2023/day-13/solution.py
+++ 2023/day-13/solution.py
@@ -41,9 +41,9 @@ def find_vertical(pattern):
     return None
 
 
-def part1(pattern):
-    h = find_horizontal(pattern)
-    v = find_vertical(pattern)
+def get_value(pattern, h_func, v_func):
+    h = h_func(pattern)
+    v = v_func(pattern)
     if h:
         return h * 100
     if v:
@@ -51,4 +51,42 @@ def part1(pattern):
     return 0
 
 
-print(f"Part 1: {sum([part1(p) for p in patterns])}")
+part1 = sum([get_value(p, find_horizontal, find_vertical) for p in patterns])
+print(f"Part 1: {part1}")
+
+
+def diff(seq1, seq2):
+    return sum([0 if seq1[i] == seq2[i] else 1 for i in range(len(seq1))])
+
+
+def diff_horizontal(pattern, row):
+    # minimum distance above or below mirror
+    m = min(row, len(pattern) - row)
+
+    return sum([diff(pattern[row - offset - 1], pattern[row + offset]) for offset in range(m)])
+
+
+def horizontal_smudge(pattern):
+    for row in range(1, len(pattern)):
+        if diff_horizontal(pattern, row) == 1:
+            return row
+    return None
+
+
+def diff_vertical(pattern, col):
+    # minimum distance left or right of mirror
+    m = min(col, len(pattern[0]) - col)
+
+    return sum(
+        [diff(get_vertical(pattern, col - offset - 1), get_vertical(pattern, col + offset)) for offset in range(m)])
+
+
+def vertical_smudge(pattern):
+    for col in range(1, len(pattern[0])):
+        if diff_vertical(pattern, col) == 1:
+            return col
+    return None
+
+
+part2 = sum([get_value(p, horizontal_smudge, vertical_smudge) for p in patterns])
+print(f"Part 2: {part2}")
