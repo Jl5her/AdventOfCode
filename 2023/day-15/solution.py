19665b2ee93cb13e0c19f07d7a75a4d0652b1402
Jack Pfeiffer <jl5her@yahoo.com>
2023-12-15 00:48:32 -0500

    2023 Day 15 Part 2

--- 2023/day-15/solution.py
+++ 2023/day-15/solution.py
@@ -8,3 +8,40 @@ part1 = sum(map(lambda word: functools.reduce(lambda v, c: ((v + ord(c)) * 17) %
 
 print(f"Part 1: {part1}")
 
+# Part 2
+boxes = {}
+for i in range(256):
+    boxes[str(i)] = []
+
+for word in words:
+    op = "=" if "=" in word else "-"
+    op_index = word.index(op)
+
+    label = word[:op_index]
+    focal_length = word[op_index + 1:]
+
+    box = str(functools.reduce(lambda v, c: ((v + ord(c)) * 17) % 256, label, 0))
+
+    if op == "=":
+        index = -1
+        for i, l in enumerate(boxes[box]):
+            if label in l:
+                index = i
+
+        lens = f"{label} {focal_length}"
+
+        if index == -1:
+            boxes[box].append(lens)
+        else:
+            boxes[box][index] = lens
+    elif op == "-":
+        boxes[box] = [b for b in boxes[box] if label not in b]
+
+part2 = 0
+for b,box in boxes.items():
+    if len(box) != 0:
+        for l, lens in enumerate(box):
+            part2 += (int(b) + 1) * (l + 1) * int(lens.split(' ')[1])
+
+
+print(f"Part 2: {part2}")
